f1ed1c08a75db05ad3424ffdef3b2f69cca5e813
pgajowniczek <pgajowniczek123@gmail.com>
2023-01-17 16:53:55 +0100

    Day 4

--- CSharpApp/CSharpApp/Program.cs
+++ CSharpApp/CSharpApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("TEST TEST TEST");
+﻿var name = "Ewa";
+var gender = 'K';
+var age = 20;
+
+if (gender == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+
+if(age < 18 && gender == 'M')
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
