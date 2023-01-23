73fdceabc66c461c60daec2e30d32dd607929bdf
pgajowniczek <pgajowniczek123@gmail.com>
2023-01-24 00:10:24 +0100

    Highest score Program

---
+++ CSharpApp/CSharpApp/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+namespace CSharpApp
+{
+    internal class Employee
+    {
+        static int highestScore = 0;
+        static Employee highestScoreEmployee;
+        public Employee(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public int Age { get; private set; }
+
+        private List<int> score = new List<int>();
+
+        public void AddScore(int value)
+        {
+            score.Add(value);
+            if (score.Sum() > highestScore)
+            {
+                highestScore = score.Sum();
+                highestScoreEmployee = this;
+            }
+        }
+        public static void ShowHighestScoreUser()
+        {
+            Console.WriteLine($"Użytkownik z najwyższą liczbą punktów: \n\nImię: {highestScoreEmployee.Name}\nNazwisko: {highestScoreEmployee.Surname}\nLiczba punktów: {highestScore}");
+        }
+
+    }
+}
--- CSharpApp/CSharpApp/Program.cs
+++ CSharpApp/CSharpApp/Program.cs
@@ -1,18 +1,28 @@
-﻿var name = "Ewa";
-var gender = 'K';
-var age = 20;
+﻿using CSharpApp;
 
-if (gender == 'K' && age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
+Employee user1 = new Employee("Marek", "Adamiak", 23);
+Employee user2 = new Employee("Irena", "Nowak", 25);
+Employee user3 = new Employee("Jan", "Kowalski", 35);
 
-if(name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat 30");
-}
+user1.AddScore(2);
+user1.AddScore(6);
+user1.AddScore(4);
+user1.AddScore(5);
+user1.AddScore(7);
+//24
 
-if(age < 18 && gender == 'M')
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
+user2.AddScore(1);
+user2.AddScore(3);
+user2.AddScore(2);
+user2.AddScore(2);
+user2.AddScore(5);
+//13
+
+user3.AddScore(10);
+user3.AddScore(9);
+user3.AddScore(8);
+user3.AddScore(8);
+user3.AddScore(10);
+//45
+
+Employee.ShowHighestScoreUser();
