9470eab93959556e53783cdf40a73158ad04b658
pgajowniczek <pgajowniczek123@gmail.com>
2023-02-08 20:08:10 +0100

    New Statistics options

--- CSharpApp/CSharpApp/Program.cs
+++ CSharpApp/CSharpApp/Program.cs
@@ -1,39 +1,42 @@
 ﻿using CSharpApp;
 
-<<<<<<< HEAD
+
 var employee = new Employee("Piotr", "Gajowniczek");
+employee.AddGrade(1);
 employee.AddGrade(2);
-employee.AddGrade(2);
-employee.AddGrade(6);
+employee.AddGrade(9);
+Console.WriteLine("Zwykłe: ");
 var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
-=======
-Employee user1 = new Employee("Marek", "Adamiak", 23);
-Employee user2 = new Employee("Irena", "Nowak", 25);
-Employee user3 = new Employee("Jan", "Kowalski", 35);
 
-user1.AddScore(2);
-user1.AddScore(6);
-user1.AddScore(4);
-user1.AddScore(5);
-user1.AddScore(7);
-//24
+Console.WriteLine();
+Console.WriteLine("Foreach");
+var statistics2 = employee.GetStatisticsWithForEach();
+Console.WriteLine($"Average: {statistics2.Average:N2}");
+Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
 
-user2.AddScore(1);
-user2.AddScore(3);
-user2.AddScore(2);
-user2.AddScore(2);
-user2.AddScore(5);
-//13
+Console.WriteLine();
+Console.WriteLine("For");
+var statistics3 = employee.GetStatisticsWithFor();
+Console.WriteLine($"Average: {statistics3.Average:N2}");
+Console.WriteLine($"Min: {statistics3.Min}");
+Console.WriteLine($"Max: {statistics3.Max}");
 
-user3.AddScore(10);
-user3.AddScore(9);
-user3.AddScore(8);
-user3.AddScore(8);
-user3.AddScore(10);
-//45
+Console.WriteLine();
+Console.WriteLine("Do While");
+var statistics4 = employee.GetStatisticsWithDoWhile();
+Console.WriteLine($"Average: {statistics4.Average:N2}");
+Console.WriteLine($"Min: {statistics4.Min}");
+Console.WriteLine($"Max: {statistics4.Max}");
 
-Employee.ShowHighestScoreUser();
->>>>>>> 73fdceabc66c461c60daec2e30d32dd607929bdf
+
+Console.WriteLine();
+Console.WriteLine("While");
+var statistics5 = employee.GetStatisticsWithWhile();
+Console.WriteLine($"Average: {statistics5.Average:N2}");
+Console.WriteLine($"Min: {statistics5.Min}");
+Console.WriteLine($"Max: {statistics5.Max}");
+
